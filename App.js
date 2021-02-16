61cd80883c802f73bbbfb1074b2bff2dd4778ea0
fredysomy <fredysomy@gmail.com>
2021-02-16 07:14:43 +0000

    removed bundles

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>By Fredy Somy</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3a415e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+      color:"cyan",
+      fontSize:"30"
+  }
 });
